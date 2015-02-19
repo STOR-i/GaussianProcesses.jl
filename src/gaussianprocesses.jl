982a0f36fd9e0d78343e100464b7653d69c4e4b3
Christopher Nemeth <christopher.nemeth@hotmail.com>
2015-02-19 12:21:36 +0000

    Added mean function to called functions

--- src/gaussianprocesses.jl
+++ src/gaussianprocesses.jl
@@ -8,7 +8,7 @@ export GP, predict, rbf, exf, gef, mat32, mat52, meanZero
 # all package code should be included here
 include("GP.jl")
 #include("expected_improvement.jl")
-#include("mean_functions.jl")
+include("mean_functions.jl")
 include("kernels.jl")
 
 end # module
