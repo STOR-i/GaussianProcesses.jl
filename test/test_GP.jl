27ca3efe7e365264020f6cc5019b4e71d84e7fa5
Jamie Fairbrother <jfairbrother87@hotmail.com>
2015-02-22 16:21:30 +0000

    Updated test_GP to check predict provides correct means at input observations

--- test/test_GP.jl
+++ test/test_GP.jl
@@ -1,13 +1,16 @@
 using gaussianprocesses
 
-# 1-dimensional case
-xpred = [-4.0,-3.0,-1.0, 0.0, 2.0]
-y = [-2.0, 0.0, 1.0, 2.0, -1.0]
-gp = GP(xpred,y,meanZero,rbf)
-predict(gp, xpred)
+d, n = 10, 20
 
-# 2-dimensional case
-xpred = [[0.0, 0.0] [1.0, 0.0] [0.0, 1.0] [1.0, 1.0] [0.5, 0.5]]
-y = [-2.0, 0.0, 1.0, 2.0, -1.0]
-gp = GP(xpred,y,meanZero,rbf)
-predict(gp, xpred)
+x = 2π * rand(d, n)
+y = Float64[sum(sin(x[:,i])) for i in 1:n]/d
+gp = GP(x, y, meanZero, rbf)
+
+# Function verifies that predictive mean at input observations
+# are the same as the output observations
+function test_pred_matches_obs(gp::GP)
+    y_pred, sig = predict(gp, x)
+    @test_approx_eq_eps maximum(abs(gp.y - y_pred)) 0.0 1e-12
+end
+
+test_pred_matches_obs(gp)
