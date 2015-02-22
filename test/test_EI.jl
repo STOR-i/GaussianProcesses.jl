329d2f3188e7bda3c62d9bda512e508d126923f3
Jamie Fairbrother <jfairbrother87@hotmail.com>
2015-02-22 16:29:25 +0000

    Added test_EI.jl

---
+++ test/test_EI.jl
@@ -0,0 +1,15 @@
+using gaussianprocesses
+
+d, n = 10, 20
+x = 2π * rand(d, n)
+y = Float64[sum(sin(x[:,i])) for i in 1:n]/d
+gp = GP(x, y, meanZero, rbf)
+
+# Function verifies that predictive mean at input observations
+# are the same as the output observations
+function test_EI_zero_at_obs(gp::GP)
+    ei = EI(gp, gp.x)
+    @test maximum(ei) < 1e-12
+end
+
+test_pred_matches_obs(gp)
