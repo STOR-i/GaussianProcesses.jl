566f9149deba52933932e1c6d66662f128ac5f0f
Christopher Nemeth <christopher.nemeth@hotmail.com>
2015-02-24 15:20:57 +0000

    Changed example

--- test/example_1.jl
+++ test/example_1.jl
@@ -15,14 +15,15 @@ function plot(gp::GP, x::Array{Float64})
 end
 
 #Training data
-x = [-4.0,-3.0,-1.0, 0.0, 2.0];       
-y = [-2.0, 0.0, 1.0, 2.0, -1.0];
+x = rand(Uniform(-5,5),5);
+y = sin(x) + rand(Normal(0,0.5),5);
+
 #Test data
 xpred = [-5:0.1:5];
 
-#Specify covariance function
+#Specify covariance function, not that default hyperparameters are l=1 and sigma²=1
 mat32 = MAT32()
 
-gp = GP(x,y,meanZero,mat32)
+gp = GP(x,y,meanZero,mat32,0.5)
 predict(gp, xpred)
 plot(gp, xpred)
