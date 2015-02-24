1a3cf85806f1d6cda74834d5987ecfb53a1ad99f
Christopher Nemeth <christopher.nemeth@hotmail.com>
2015-02-24 09:43:00 +0000

    Fixed kernel and changed rbf to se

--- src/kernels.jl
+++ src/kernels.jl
@@ -2,20 +2,20 @@
 
 #See Chapter 4 Page 90 of Rasumussen and Williams Gaussian Processes for Machine Learning
 
-# Radial Basic Function 
-rbf(x::Vector{Float64}, y::Vector{Float64}, l::Float64=1.0) =  exp(-0.5 * norm(x-y)^2/l^2)
+# Squared Exponential Function 
+se(x::Vector{Float64}, y::Vector{Float64}, hyp::Vector{Float64}) =  exp(-hyp[2]*norm(x-y)^2/hyp[1]^2)
 
 #Exponential Function
-exf(x::Vector{Float64}, y::Vector{Float64}, l::Float64=1.0) =  exp(-norm(x-y)/l^2)
+exf(x::Vector{Float64}, y::Vector{Float64}, hyp::Vector{Float64}) =  exp(-norm(x-y)/hyp^2)
 
 #Gamma Exponential Function
-gef(x::Vector{Float64}, y::Vector{Float64}, l::Float64=1.0, gamma::Float64=1.0) =  exp(-(norm(x-y)/l)^gamma)    
+gef(x::Vector{Float64}, y::Vector{Float64}, hyp::Vector{Float64}) =  exp(-(norm(x-y)/hyp[1])^hyp[2])    
 
 #Matern 3/2 Function
-mat32(x::Vector{Float64}, y::Vector{Float64}, l::Float64=1.0) =  (1+sqrt(3)*norm(x-y)/l)*exp(-sqrt(3)*norm(x-y)/l)    
+mat32(x::Vector{Float64}, y::Vector{Float64}, hyp::Vector{Float64}) =  hyp[2]*(1+sqrt(3*norm(x-y)^2)/hyp[1])*exp(-sqrt(3*norm(x-y)^2)/hyp[1])    
 
 #Matern 5/2 Function
-mat52(x::Vector{Float64}, y::Vector{Float64}, l::Float64=1.0) =  (1+sqrt(5)*norm(x-y)/l+sqrt(5)*norm(x-y)^2/(3*l^2))*exp(-sqrt(5)*norm(x-y)/l)    
+mat52(x::Vector{Float64}, y::Vector{Float64}, hyp::Vector{Float64}) =  hyp[2]*(1+sqrt(5*norm(x-y)^2)/hyp[1]+sqrt(5*norm(x-y)^2)/(3*hyp[1]^2))*exp(-sqrt(5*norm(x-y)^2)/hyp[1])    
 
 
 
