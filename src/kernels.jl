3b77e71bdbf70450d3e64e5f2c3f1deec40285cd
Christopher Nemeth <christopher.nemeth@hotmail.com>
2015-03-03 20:43:53 +0000

    Added gradients for matern 3/2

--- src/kernels.jl
+++ src/kernels.jl
@@ -27,20 +27,27 @@ grad_kern(se::SE, x::Vector{Float64}, y::Vector{Float64}) = [se.σ^2*norm(x-y)^2
 #Matern 3/2 Function
 type MAT32 <: Kernel
     l::Float64      #Length scale 
-    σ²::Float64     #Signal variance
-    MAT32(l::Float64=1.0, σ²::Float64=1.0) = new(l, σ²)
+    σ::Float64      #Signal std
+    MAT32(l::Float64=1.0, σ::Float64=1.0) = new(l, σ)
 end
 
-kern(mat32::MAT32, x::Vector{Float64}, y::Vector{Float64}) = mat32.σ²*(1+sqrt(3*norm(x-y)^2)/mat32.l)*exp(-sqrt(3*norm(x-y)^2)/mat32.l)
-params(mat32::MAT32) = (mat32.l, mat32.σ²)
+kern(mat32::MAT32, x::Vector{Float64}, y::Vector{Float64}) = mat32.σ^2*(1+sqrt(3)*norm(x-y)/mat32.l)*exp(-sqrt(3)*norm(x-y)/mat32.l)
+params(mat32::MAT32) = Float64[mat32.l, mat32.σ]
+num_params(mat32::MAT32) = 2
+function set_params!(mat32::MAT32, hyp::Vector{Float64})
+    length(hyp) == 2 || throw(ArgumentError("Matern 3/2 only has two parameters"))
+    mat32.l, mat32.σ = hyp
+end
+grad_kern(mat32::MAT32, x::Vector{Float64}, y::Vector{Float64}) = [mat32.σ^2*(sqrt(3)*norm(x-y)/mat32.l)^2*exp(-sqrt(3)*norm(x-y)/mat32.l), 2.0*mat32.σ*(1+sqrt(3)*norm(x-y)/mat32.l)*exp(-sqrt(3)*norm(x-y)/mat32.l)]
+
 
 
 #Matern 5/2 Function
 type MAT52 <: Kernel
     l::Float64      #Length scale 
-    σ²::Float64     #Signal variance
-    MAT52(l::Float64=1.0, σ²::Float64=1.0) = new(l, σ²)
+    σ::Float64     #Signal std
+    MAT52(l::Float64=1.0, σ::Float64=1.0) = new(l, σ)
 end
 
-kern(mat52::MAT52, x::Vector{Float64}, y::Vector{Float64}) = mat52.σ²*(1+sqrt(5*norm(x-y)^2)/mat52.l+sqrt(5*norm(x-y)^2)/(3*mat52.l^2))*exp(-sqrt(5*norm(x-y)^2)/mat52.l)   
-params(mat52::MAT52) = (mat52.l, mat52.σ²)
+kern(mat52::MAT52, x::Vector{Float64}, y::Vector{Float64}) = mat52.σ^2*(1+sqrt(5)*norm(x-y)/mat52.l+sqrt(5)*norm(x-y)/(3*mat52.l^2))*exp(-sqrt(5)*norm(x-y)/mat52.l)   
+params(mat52::MAT52) = Float64[mat52.l, mat52.σ]
--- test/example_3.jl
+++ test/example_3.jl
@@ -8,8 +8,8 @@ x = 2*π*rand(5);
 y = cos(x) + 0.5*randn(5);
 
 #Specify covariance function, not that default hyperparameters are l=1 and sigma²=1
-se = SE()
+mat32 = MAT32()
 
-gp = GP(x,y,meanZero,se)
+gp = GP(x,y,meanZero,mat32)
 optimize!(gp)
 
