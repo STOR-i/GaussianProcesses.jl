2662cfa1444ac31a078394729e6304e783ca7730
Jamie Fairbrother <jfairbrother87@hotmail.com>
2015-03-05 17:48:32 +0000

    Minor update to example_3.jl

--- test/example_3.jl
+++ test/example_3.jl
@@ -7,12 +7,9 @@ using gaussianprocesses
 x=[-4.0,-3.0,-1.0,0.0,2.0];
 y=[-2.0,0.0,1.0,2.0,-1.0];
 
-#Test data
-xpred = [-5.0:0.1:5.0];
-
 #Specify covariance function, not that default hyperparameters are l=1 and sigma²=1
 se = SE()
 
 gp = GP(x,y,meanZero,se)
-optimize!(gp)
+optimize!(gp, method=:bfgs, show_trace=true)
 
