2560e7976cb87f2db8e5a35ccacc93ae44325f32
Christopher Nemeth <christopher.nemeth@hotmail.com>
2015-03-05 20:49:33 +0000

    Modified Mat32 kernel

--- src/kernels.jl
+++ src/kernels.jl
@@ -17,7 +17,8 @@ function kern(se::SE, x::Vector{Float64}, y::Vector{Float64})
     ell = exp(se.ll)
     sigma2 = exp(2*se.lσ)
     
-    sigma2*exp(-0.5*norm(x-y)^2/ell^2) 
+    K = sigma2*exp(-0.5*norm(x-y)^2/ell^2)
+    return K
 end    
 params(se::SE) = exp(Float64[se.ll, se.lσ])
 num_params(se::SE) = 2
@@ -39,20 +40,33 @@ end
 
 # Matern 3/2 Function
 type MAT32 <: Kernel
-    l::Float64      # Length scale 
-    σ::Float64      # Signal std
-    MAT32(l::Float64=1.0, σ::Float64=1.0) = new(l, σ)
+    ll::Float64      # Log of Length scale 
+    lσ::Float64      # Log of Signal std
+    MAT32(l::Float64=1.0, σ::Float64=1.0) = new(log(l),log(σ))
 end
 
-kern(mat32::MAT32, x::Vector{Float64}, y::Vector{Float64}) = mat32.σ^2*(1+sqrt(3)*norm(x-y)/mat32.l)*exp(-sqrt(3)*norm(x-y)/mat32.l)
-params(mat32::MAT32) = Float64[mat32.l, mat32.σ]
+function kern(mat32::MAT32, x::Vector{Float64}, y::Vector{Float64})
+    ell = exp(mat32.ll)
+    sigma2 = exp(2*mat32.lσ)
+    
+    K = sigma2*(1+sqrt(3)*norm(x-y)/ell)*exp(-sqrt(3)*norm(x-y)/ell)
+    return K
+end    
+params(mat32::MAT32) = exp(Float64[mat32.ll, mat32.lσ])
 num_params(mat32::MAT32) = 2
 function set_params!(mat32::MAT32, hyp::Vector{Float64})
     length(hyp) == 2 || throw(ArgumentError("Matern 3/2 only has two parameters"))
-    mat32.l, mat32.σ = hyp
+    mat32.ll, mat32.lσ = hyp
 end
-grad_kern(mat32::MAT32, x::Vector{Float64}, y::Vector{Float64}) = [mat32.σ^2*(sqrt(3)*norm(x-y)/mat32.l)^2*exp(-sqrt(3)*norm(x-y)/mat32.l), 2.0*mat32.σ*(1+sqrt(3)*norm(x-y)/mat32.l)*exp(-sqrt(3)*norm(x-y)/mat32.l)]
-
+function grad_kern(mat32::MAT32, x::Vector{Float64}, y::Vector{Float64})
+    ell = exp(mat32.ll)
+    sigma2 = exp(2*mat32.lσ)
+    
+    dK_ell = sigma2*(sqrt(3)*norm(x-y)/ell)^2*exp(-sqrt(3)*norm(x-y)/ell)
+    dK_sigma = 2.0*sigma2*(1+sqrt(3)*norm(x-y)/ell)*exp(-sqrt(3)*norm(x-y)/ell)
+    dK_theta = [dK_ell,dK_sigma]
+    return(dK_theta)
+end    
 
 
 # Matern 5/2 Function
