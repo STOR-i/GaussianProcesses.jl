5c6e5408fa449b0e97bde2c9610aec9e7a5e5973
Christopher Nemeth <christopher.nemeth@hotmail.com>
2015-03-06 15:03:39 +0000

    added some labels

--- src/kernels/kernels.jl
+++ src/kernels/kernels.jl
@@ -1,17 +1,15 @@
 import Distributions.params
 
-# Here will go built-in covariance functions
-
 # See Chapter 4 Page 90 of Rasumussen and Williams Gaussian Processes for Machine Learning
 
 abstract Kernel
 
-include("se.jl")
-include("mat32.jl")
-include("mat52.jl")
-include("exf.jl")
-include("periodic.jl")
-include("poly.jl")
+include("se.jl")                # Squared exponential covariance function
+include("mat32.jl")             # Matern 3/2 covariance function
+include("mat52.jl")             # Matern 5/2 covariance function
+include("exf.jl")               # Exponential covariance function
+include("periodic.jl")          # Periodic covariance function
+include("poly.jl")              # Polnomial covariance function
 
 
 
