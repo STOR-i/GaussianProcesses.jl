16adfeb5178a0926b8a26f6ea3f141b6b34094a9
Jamie Fairbrother <jfairbrother87@hotmail.com>
2015-03-12 17:36:27 +0000

    Fixed mPOLY functions (except for grad_kern)

--- src/means/mPoly.jl
+++ src/means/mPoly.jl
@@ -1,35 +1,38 @@
-#Polynomial mean function
+# Polynomial mean function
 
 type mPOLY <: Mean
-    β::Vector{Float64}    #Polynomial coefficients
-    d::Int64              #Polynomial degree
-    mPOLY(β::Vector{Float64},d::Int64) = new(β,d)
-end    
-    
+    β::Matrix{Float64}    # Polynomial coefficients
+    dim::Int              # Polynomial degree
+    deg::Int              # Dimension
+    mPOLY(β::Vector{Float64}) = new(β,size(β, 1), size(β, 2))
+end
+
 function meanf(mPoly::mPOLY,x::Matrix{Float64})
     dim, nobsv = size(x)
-    z = zeros(nobsv,1)
-    beta = reshape(mPoly.β,dim,mPoly.d)
-    for i in 1:mPoly.d
-        z = z + x'.^i*beta[:,i]
+    dim == mPoly.dim || throw(ArgumentError("Observations and mean function have inconsistent dimensions"))
+    z = zeros(nobsv)
+    for i in 1:nobsv
+        for j in 1:mPoly.deg
+            z[i] += dot(x[:, i].^j, mPoly.β[:j])
     end
     return z
-end    
+end
 
-params(mPoly::mPOLY) = Float64[mPoly.β]
-num_params(mPoly::mPOLY) = gp.dim
+params(mPoly::mPOLY) = vec(mPoly.β)
+num_params(mPoly::mPOLY) = mPoly.dim * mPoly.deg
 function set_params!(mPoly::mPOLY, hyp::Vector{Float64})
-    length(hyp) == gp.dim || throw(ArgumentError("Polynomial mean function only has $(gp.dim) parameters"))
+    num_param = mPoly.dim * mPoly.deg
+    length(hyp) == num_param || throw(ArgumentError("Polynomial mean function has $(num_param) parameters"))
     mPoly.β = hyp
 end
 
+# Needs fixing...
 function grad_meanf(mPoly::mPOLY, x::Vector{Float64})
-      nobsv = size(x,2)
-      z = zeros(nobsv,1)
+    nobsv = size(x,2)
+    z = zeros(nobsv,1)
     for i in 1:mPoly.d
      z = z + x'.^i
     end
     dM_theta = z
     return dM_theta
-end    
-
+end
