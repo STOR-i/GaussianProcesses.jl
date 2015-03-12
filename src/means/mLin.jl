40e57293c5d62e703fd665efa71132bdccb9f2be
Jamie Fairbrother <jfairbrother87@hotmail.com>
2015-03-12 17:24:15 +0000

    Fixed mLin.jl

--- src/means/mLin.jl
+++ src/means/mLin.jl
@@ -1,20 +1,20 @@
-#Linear mean function
+# Linear mean function
 
 type mLIN <: Mean
     β::Vector{Float64}
-    mLIN(β::Vector{Float64}) = new(β)
-end    
+    dim::Int
+    mLIN(β::Vector{Float64}) = new(β, length(β)
+end
     
 meanf(mLin::mLIN,x::Matrix{Float64}) =  x'*mLin.β
 
-params(mLin::mLIN) = Vector{Float64}[mLin.β]
-num_params(mLin::mLIN) = gp.dim
+params(mLin::mLIN) = mLin.β
+num_params(mLin::mLIN) = mLin.dim
 function set_params!(mLin::mLIN, hyp::Vector{Float64})
-    length(hyp) == gp.dim || throw(ArgumentError("Linear mean function only has $(gp.dim) parameters"))
+    length(hyp) == mLin.dim || throw(ArgumentError("Linear mean function only has $(gp.dim) parameters"))
     mLin.β = hyp
 end
 function grad_meanf(mLin::mLIN, x::Vector{Float64})
     dM_theta = x
     return dM_theta
-end    
-
+end
