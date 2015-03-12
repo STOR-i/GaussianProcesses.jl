e6e09c73a7e2d60e26120a0824ab92bbd5633dbb
Jamie Fairbrother <jfairbrother87@hotmail.com>
2015-03-12 17:24:00 +0000

    *** empty log message ***

--- src/means/mConst.jl
+++ src/means/mConst.jl
@@ -12,11 +12,10 @@ meanf(mConst::mCONST,x::Matrix{Float64}) =  fill(mConst.β, size(x,2))
 params(mConst::mCONST) = Float64[mConst.β]
 num_params(mConst::mCONST) = gp.dim
 function set_params!(mConst::mCONST, hyp::Vector{Float64})
-    length(hyp) == gp.dim || throw(ArgumentError("Constant mean function only has $(gp.dim) parameters"))
+    length(hyp) == 1 || throw(ArgumentError("Constant mean function only has 1 parameter"))
     mConst.β = hyp
 end
 function grad_meanf(mConst::mCONST, x::Vector{Float64})
     dM_theta = ones(size(x,2))
     return dM_theta
-end    
-
+end
