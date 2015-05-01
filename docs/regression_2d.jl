0bed00f061e0d0828265cdc382f8770d72474aa6
Christopher Nemeth <christopher.nemeth@hotmail.com>
2015-05-01 14:33:13 +0100

    Added 2d plot

--- docs/regression_2d.jl
+++ docs/regression_2d.jl
@@ -3,17 +3,17 @@
 using Gadfly, GaP
 
 #Training data
-d, n = 2, 50
-
-x = 2π * rand(d, n)
+d, n = 2, 50         #Dimension and number of observations
+ 
+x = 2π * rand(d, n)                               
 y = vec(sin(x[1,:]).*sin(x[2,:])) + 0.05*rand(n) 
 
-mZero = MeanZero()
-kern = Mat(5/2,[0.0,0.0],0.0)
-gp = GP(x,y,mZero,kern,-2.0)
-optimize!(gp)
+mZero = MeanZero()                    #Zero mean function
+kern = Mat(5/2,[0.0,0.0],0.0)         #Matern 5/2 ARD kernel with parameters log(l₁) = 0 log(l₂) = 0 and log(σ) = 0
+gp = GP(x,y,mZero,kern,-2.0)          # Fit the GP
+optimize!(gp)                         # Optimize the hyperparameters
 
-plot(gp; clim=(-10.0, 10.0,-10.0,10.0))
+plot(gp; clim=(-10.0, 10.0,-10.0,10.0)) #Plot the GP over range clim
      
 
 
