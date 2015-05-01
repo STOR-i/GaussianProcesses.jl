eb6a170014584e16dbe6240a5c98ce57118c4c1b
Christopher Nemeth <christopher.nemeth@hotmail.com>
2015-05-01 15:36:17 +0100

    updated 2d example

--- docs/regression_2d.jl
+++ docs/regression_2d.jl
@@ -9,7 +9,7 @@ x = 2π * rand(d, n)
 y = vec(sin(x[1,:]).*sin(x[2,:])) + 0.05*rand(n) 
 
 mZero = MeanZero()                    #Zero mean function
-kern = Mat(5/2,[0.0,0.0],0.0)         #Matern 5/2 ARD kernel with parameters log(l₁) = 0 log(l₂) = 0 and log(σ) = 0
+kern = Mat(5/2,[0.0,0.0],0.0) + SE(0.0,0.0)        #Matern 5/2 ARD kernel with parameters log(l₁) = 0 log(l₂) = 0 and log(σ) = 0 and SE Iso kernel with parameters log(ℓ) = 0 and log(σ) = 0
 gp = GP(x,y,mZero,kern,-2.0)          # Fit the GP
 optimize!(gp)                         # Optimize the hyperparameters
 
