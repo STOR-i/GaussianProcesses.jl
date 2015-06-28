30bf4ba5fb966e6d21058b00b53725cefc8619aa
Jamie Fairbrother <jfairbrother87@hotmail.com>
2015-06-28 13:04:19 +0100

    Added performance test for composite kernels

---
+++ perf/kernels/composite_perf.jl
@@ -0,0 +1,20 @@
+require("kern_proc.jl")
+
+d = 10
+n = [10, 50, 100, 500]
+
+cross_kern_procs = Array(Proc, 0)
+push!(cross_kern_procs, KernelTest(SEIso(1.0, 1.0) + RQIso(1.0, 1.0, 1.0), d, GaussianProcesses.crossKern))
+push!(cross_kern_procs, KernelTest(SEIso(1.0, 1.0) * RQIso(1.0, 1.0, 1.0), d, GaussianProcesses.crossKern))
+cross_kern_table = run(cross_kern_procs, n)
+
+grad_stack_procs = Array(Proc, 0)
+push!(grad_stack_procs, KernelTest(SEIso(1.0, 1.0) + RQIso(1.0, 1.0, 1.0), d, GaussianProcesses.grad_stack))
+push!(grad_stack_procs, KernelTest(SEIso(1.0, 1.0) * RQIso(1.0, 1.0, 1.0), d, GaussianProcesses.grad_stack))
+grad_stack_table = run(grad_stack_procs, n)
+
+println("\ncrossKern calculations")
+show(cross_kern_table)
+
+println("\grad_stack calculations")
+show(grad_stack_table)
