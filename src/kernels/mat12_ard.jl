150e319279102aec92af239d926c31bc41d07ba5
Chris Nemeth <nemeth@fyb064000012.lancs.ac.uk>
2015-09-28 14:22:57 +0100

    completed the conversion of ARD kernels to stationary framework

--- src/kernels/mat12_ard.jl
+++ src/kernels/mat12_ard.jl
@@ -9,39 +9,32 @@ k(x,x') = σ²exp(-d/L), where d = |x-x'| and L = diag(ℓ₁,ℓ₂,...)
 * `ll::Vector{Float64}`: Log of the length scale ℓ
 * `lσ::Float64`: Log of the signal standard deviation σ
 """ ->
-type Mat12Ard <: Kernel
-    ll::Vector{Float64}     # Log of length scale
-    lσ::Float64             # Log of signal std
+type Mat12Ard <: Stationary
+    ℓ::Vector{Float64}     # Log of length scale
+    σ2::Float64             # Log of signal std
     dim::Int                # Number of hyperparameters
-    Mat12Ard(ll::Vector{Float64}, lσ::Float64) = new(ll,lσ, size(ll,1)+1)
+    Mat12Ard(ll::Vector{Float64}, lσ::Float64) = new(exp(ll),exp(2.0*lσ), size(ll,1)+1)
 end
-
-function kern(mat::Mat12Ard, x::Vector{Float64},y::Vector{Float64})
-    ell = exp(mat.ll)
-    sigma2 = exp(2*mat.lσ)
-    K = sigma2*exp(-weuclidean(x,y, 1.0./(ell.^2)))
-    return K
-end
-
-get_params(mat::Mat12Ard) = [mat.ll, mat.lσ]
-
-num_params(mat::Mat12Ard) = mat.dim
 
 function set_params!(mat::Mat12Ard, hyp::Vector{Float64})
     length(hyp) == mat.dim || throw(ArgumentError("Matern 1/2 covariance function only has $(mat.dim) parameters"))
-    mat.ll = hyp[1:(mat.dim-1)]
-    mat.lσ = hyp[mat.dim]
+    mat.ℓ  = exp(hyp[1:(mat.dim-1)])
+    mat.σ2 = exp(2.0*hyp[mat.dim])
 end
 
+get_params(mat::Mat12Ard) = [log(mat.ℓ), log(mat.σ2)/2.0]
+num_params(mat::Mat12Ard) = mat.dim
+
+metric(mat::Mat12Ard) = WeightedEuclidean(1.0./(mat.ℓ))
+kern(mat::Mat12Ard, r::Float64) = mat.σ2*exp(-r)
+
 function grad_kern(mat::Mat12Ard, x::Vector{Float64}, y::Vector{Float64})
-    ell = exp(mat.ll)
-    sigma2 = exp(2*mat.lσ)
-    wdiff = (x-y)./ell
-    dxy = norm(wdiff)
+    r = distance(mat, x, y)
+    exp_r = exp(-0.5*r)
+    wdiff = (x-y)./mat.ℓ
     
-    dK_ell = sigma2*wdiff*exp(-dxy)
-    dK_sigma = 2.0*sigma2*exp(-dxy)
-    dK_theta = [dK_ell,dK_sigma]
+    g1 = mat.σ2*wdiff*exp_r
+    g2 = 2.0*mat.σ2*exp_r
     
-    return dK_theta
+    return [g1, g2]
 end
--- src/kernels/rq_ard.jl
+++ src/kernels/rq_ard.jl
@@ -10,46 +10,38 @@ k(x,x') = σ²(1+(x-x')ᵀL⁻²(x-x')/2α)^{-α}, where L = diag(ℓ₁,ℓ₂,
 * `lσ::Float64`        : Log of the signal standard deviation σ
 * `lα::Float64`        : Log of shape parameter α
 """ ->
-type RQArd <: Kernel
-    ll::Vector{Float64}      # Log of length scale 
-    lσ::Float64              # Log of signal std
-    lα::Float64              # Log of shape parameter
+type RQArd <: Stationary
+    ℓ2::Vector{Float64}      # Log of length scale 
+    σ2::Float64              # Log of signal std
+    α::Float64              # Log of shape parameter
     dim::Int                 # Number of hyperparameters
-    RQArd(ll::Vector{Float64}, lσ::Float64, lα::Float64) = new(ll, lσ, lα, size(ll,1)+2)
+    RQArd(ll::Vector{Float64}, lσ::Float64, lα::Float64) = new(exp(2.0*ll), exp(2.0*lσ), exp(lα), size(ll,1)+2)
 end
-
-function kern(rq::RQArd, x::Vector{Float64}, y::Vector{Float64})
-    ℓ    = exp(rq.ll)
-    σ2 = exp(2*rq.lσ)
-    α  = exp(rq.lα)
-    #K =  σ2*(1+0.5*(norm((x-y)./ℓ)^2)/α)^(-α)
-    K =  σ2*(1+0.5*(wsqeuclidean(x,y,1.0./(ℓ.^2)))/α)^(-α)
-    return K
-end
-
-get_params(rq::RQArd) = [rq.ll, rq.lσ, rq.lα]
-num_params(rq::RQArd) = rq.dim
 
 function set_params!(rq::RQArd, hyp::Vector{Float64})
     length(hyp) == rq.dim || throw(ArgumentError("Rational Quadratic ARD function has $(rq.dim) parameters"))
-    rq.ll = hyp[1:rq.dim-2]
-    rq.lσ = hyp[rq.dim-1]
-    rq.lα = hyp[rq.dim]
+    rq.ℓ2 = exp(2.0*hyp[1:rq.dim-2])
+    rq.σ2 = exp(2.0*hyp[rq.dim-1])
+    rq.α = exp(hyp[rq.dim])
 end
 
-function grad_kern(rq::RQArd, x::Vector{Float64}, y::Vector{Float64})
-    ℓ    = exp(rq.ll)
-    σ2 = exp(2*rq.lσ)
-    α  = exp(rq.lα)
+get_params(rq::RQArd) = [log(rq.ℓ2)/2.0, log(rq.σ2)/2.0, log(rq.α)]
+num_params(rq::RQArd) = rq.dim
 
-    wdiff = ((x-y)./ℓ).^2
+metric(rq::RQArd) = WeightedSqEuclidean(1.0./(rq.ℓ2))
+kern(rq::RQArd,r::Float64) = rq.σ2*(1+0.5*r/rq.α)^(-rq.α)
+    
+
+function grad_kern(rq::RQArd, x::Vector{Float64}, y::Vector{Float64})
+    r = distance(rq, x, y)
+
+    wdiff = ((x-y).^2)./rq.ℓ2
     dxy2 = sum(wdiff)
     
-    dK_ℓ   = σ2*wdiff*(1+0.5*dxy2/α)^(-α-1)
-    dK_σ = 2.0*σ2*(1+0.5*dxy2/α)^(-α)
+    g1   = rq.σ2*wdiff*(1+0.5*dxy2/rq.α)^(-rq.α-1)
+    g2 = 2.0*rq.σ2*(1+0.5*dxy2/rq.α)^(-rq.α)
     
-    part     = (1+0.5*dxy2/α)
-    dK_α = σ2*part^(-α)*(0.5*dxy2/part-α*log(part))
-    dK_theta = [dK_ℓ,dK_σ,dK_α]
-    return dK_theta
+    part     = (1+0.5*dxy2/rq.α)
+    g3 = rq.σ2*part^(-rq.α)*(0.5*dxy2/part-rq.α*log(part))
+    return [g1, g2, g3]
 end
