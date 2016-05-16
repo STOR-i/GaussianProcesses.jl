83541794b973ef74712cedfd49cf53d6f0e3b0d6
nemethis <christopher.nemeth@hotmail.com>
2016-05-16 12:24:44 +0100

    fixed error with deriv in mat52_ard

--- src/kernels/mat52_ard.jl
+++ src/kernels/mat52_ard.jl
@@ -33,7 +33,7 @@ kern(mat::Mat52Ard, r::Float64) = mat.σ2*(1+sqrt(5)*r+5/3*r^2)*exp(-sqrt(5)*r)
 function grad_kern(mat::Mat52Ard, x::Vector{Float64}, y::Vector{Float64})
     r = distance(mat,x,y)
     exp_r = exp(-sqrt(5)*r)
-    wdiff = (x-y)./mat.ℓ
+    wdiff = abs(x-y)./mat.ℓ
     
     g1 = mat.σ2.*((5*wdiff.^2).*(1+sqrt(5).*wdiff)/3).*exp_r #dK_d(log ℓ)
     g2 = 2.0*mat.σ2*(1+sqrt(5)*r+5/3*r^2)*exp_r              #dK_d(log σ)
