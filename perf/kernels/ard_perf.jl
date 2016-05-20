68bd52e36e1990fbf00dbcaaccb8b0e2f1ceab1a
Jamie Fairbrother <jfairbrother87@hotmail.com>
2016-05-20 15:23:10 +0200

    Added more kernels to ard_perf.jl test

--- perf/kernels/ard_perf.jl
+++ perf/kernels/ard_perf.jl
@@ -8,12 +8,16 @@ n = [50, 100, 500]
 cross_kern_procs = Array(Proc, 0)
 push!(cross_kern_procs, KernelTest(SEArd(λ, 1.0), d, GaussianProcesses.crossKern))
 push!(cross_kern_procs, KernelTest(Mat12Ard(λ, 1.0), d, GaussianProcesses.crossKern))
+push!(cross_kern_procs, KernelTest(Mat32Ard(λ, 1.0), d, GaussianProcesses.crossKern))
+push!(cross_kern_procs, KernelTest(Mat52Ard(λ, 1.0), d, GaussianProcesses.crossKern))
 push!(cross_kern_procs, KernelTest(RQArd(λ, 1.0, 1.0), d, GaussianProcesses.crossKern))
 cross_kern_table = run(cross_kern_procs, n)
 
 grad_stack_procs = Array(Proc, 0)
 push!(grad_stack_procs, KernelTest(SEArd(λ, 1.0), d, GaussianProcesses.grad_stack))
 push!(grad_stack_procs, KernelTest(Mat12Ard(λ, 1.0), d, GaussianProcesses.grad_stack))
+push!(grad_stack_procs, KernelTest(Mat32Ard(λ, 1.0), d, GaussianProcesses.grad_stack))
+push!(grad_stack_procs, KernelTest(Mat52Ard(λ, 1.0), d, GaussianProcesses.grad_stack))
 push!(grad_stack_procs, KernelTest(RQArd(λ, 1.0, 1.0), d, GaussianProcesses.grad_stack))
 grad_stack_table = run(grad_stack_procs, n)
 
