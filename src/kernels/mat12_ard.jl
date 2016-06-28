c093d23ed4d687d4275d4b6b1d8ddb00d0db95c7
Jamie Fairbrother <jfairbrother87@hotmail.com>
2016-06-28 22:30:41 +0100

    Updated Mat12Ard to used kernel data

--- src/kernels/mat12_ard.jl
+++ src/kernels/mat12_ard.jl
@@ -4,35 +4,35 @@
 # Description
 Constructor for the ARD Matern 1/2 kernel (covariance)
 
-k(x,x') = σ²exp(-d/L), where d = |x-x'| and L = diag(ℓ₁,ℓ₂,...)
+k(x,x') = σ²exp(-d/L^2), where d = |x-x'| and L = diag(ℓ₁,ℓ₂,...)
 # Arguments:
 * `ll::Vector{Float64}`: Log of the length scale ℓ
 * `lσ::Float64`: Log of the signal standard deviation σ
 """ ->
-type Mat12Ard <: Stationary
-    ℓ::Vector{Float64}      # Log of length scale
-    σ2::Float64             # Log of signal std
-    dim::Int                # Number of hyperparameters
-    Mat12Ard(ll::Vector{Float64}, lσ::Float64) = new(exp(ll),exp(2.0*lσ), size(ll,1)+1)
+type Mat12Ard <: StationaryARD
+    ℓ2::Vector{Float64}      # Log of length scale
+    σ2::Float64              # Log of signal std
+    dim::Int                 # Number of hyperparameters
+    Mat12Ard(ll::Vector{Float64}, lσ::Float64) = new(2.0*exp(ll),exp(2.0*lσ), size(ll,1)+1)
 end
 
 function set_params!(mat::Mat12Ard, hyp::Vector{Float64})
     length(hyp) == mat.dim || throw(ArgumentError("Matern 1/2 covariance function only has $(mat.dim) parameters"))
-    mat.ℓ  = exp(hyp[1:(mat.dim-1)])
+    mat.ℓ2  = exp(2.0*hyp[1:(mat.dim-1)])
     mat.σ2 = exp(2.0*hyp[mat.dim])
 end
 
-get_params(mat::Mat12Ard) = [log(mat.ℓ); log(mat.σ2)/2.0]
-get_param_names(mat::Mat12Ard) = [get_param_names(mat.ℓ, :ll); :lσ]
+get_params(mat::Mat12Ard) = [log(mat.ℓ2)/2.0; log(mat.σ2)/2.0]
+get_param_names(mat::Mat12Ard) = [get_param_names(mat.ℓ2, :ll); :lσ]
 num_params(mat::Mat12Ard) = mat.dim
 
-metric(mat::Mat12Ard) = WeightedEuclidean(1.0./(mat.ℓ.^2))
+metric(mat::Mat12Ard) = WeightedEuclidean(1.0./(mat.ℓ2))
 cov(mat::Mat12Ard, r::Float64) = mat.σ2*exp(-r)
 
 function grad_kern(mat::Mat12Ard, x::Vector{Float64}, y::Vector{Float64})
     r = distance(mat, x, y)
     exp_r = exp(-r)
-    wdiff = ((x-y)./mat.ℓ).^2
+    wdiff = ((x-y).^2)./mat.ℓ2
     
     g1 = (mat.σ2*wdiff*exp_r)/r
     g2 = 2.0*mat.σ2*exp_r
@@ -40,16 +40,13 @@ function grad_kern(mat::Mat12Ard, x::Vector{Float64}, y::Vector{Float64})
     return [g1; g2]
 end
 
-function grad_stack!(stack::AbstractArray, X::Matrix{Float64}, mat::Mat12Ard)
+function grad_stack!(stack::AbstractArray, mat::Mat12Ard, X::Matrix{Float64}, data::StationaryARDData)
     d = size(X,1)
-    R = distance(mat,X)
-    stack[:,:,d+1] = cov(mat, X)
+    R = distance(mat, X, data)
+    stack[:,:,d+1] = mat.σ2 * exp(-R)
     ck = view(stack, :, :, d+1)
-    for i in 1:d
-        grad_ls = view(stack, :, :, i)
-        pairwise!(grad_ls, WeightedSqEuclidean([1.0/mat.ℓ[i]^2]), view(X, i, :))
-        map!(*, grad_ls, grad_ls, ck./R)
-    end
+    broadcast!(*, view(stack, :, :, 1:d), data.dist_stack, reshape(1.0./mat.ℓ2, (1,1,d)), ck)
+    broadcast!(/, view(stack, :, :, 1:d), view(stack, :, :, 1:d), R)
     stack[:,:, d+1] = 2.0 * ck
     return stack
 end
