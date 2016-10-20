15364db9f3539712c5b47e0b04d6f542c01f6df8
maximerischard <rischardmaxime@gmail.com>
2016-10-20 16:10:19 -0400

    add tests for fixed kernel and fix the bugs they found

--- test/test_optim.jl
+++ test/test_optim.jl
@@ -1,4 +1,5 @@
 using GaussianProcesses
+using GaussianProcesses: get_params, get_param_names
 
 d, n = 2, 20
 ll = rand(d)
@@ -15,5 +16,17 @@ function test_optim(kern::Kernel, X::Matrix{Float64}, y::Vector{Float64})
 	@assert gp.mLL > init_mLL
 end
 
+function test_fixed(kern::Kernel, X::Matrix{Float64}, y::Vector{Float64})
+    init_param = get_params(kern)[1]
+    fixed=fix(kern, get_param_names(kern)[1])
+	gp = GP(X,y,MeanConst(0.0), fixed, -3.0)
+	init_mLL = gp.mLL
+	optimize!(gp)
+	@assert gp.mLL > init_mLL
+	@assert get_params(kern)[1] == init_param
+end
+
 se = SEIso(1.0, 1.0)
 test_optim(se, x, y)
+rq = RQIso(1.0, 1.0, 1.0)
+test_fixed(se, x, y)
