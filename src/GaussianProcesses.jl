7500eea49f9988adad0589f42c5404de3f84c8fd
maximerischard <rischardmaxime@gmail.com>
2016-10-25 10:31:43 -0400

    remove FixedPeriodic

--- src/GaussianProcesses.jl
+++ src/GaussianProcesses.jl
@@ -9,7 +9,7 @@ import Base: rand, rand!, mean, cov, push!
 # Functions that should be available to package
 # users should be explicitly exported here
 
-export GP, predict, SumKernel, ProdKernel, Masked, FixedKern, fix, Noise, Kernel, SE, SEIso, SEArd, Periodic, FixedPeriodic, Poly, RQ, RQIso, RQArd, Lin, LinIso, LinArd, Mat, Mat12Iso, Mat12Ard, Mat32Iso, Mat32Ard, Mat52Iso, Mat52Ard, MeanZero, MeanConst, MeanLin, MeanPoly, SumMean, ProdMean, optimize!
+export GP, predict, SumKernel, ProdKernel, Masked, FixedKern, fix, Noise, Kernel, SE, SEIso, SEArd, Periodic, Poly, RQ, RQIso, RQArd, Lin, LinIso, LinArd, Mat, Mat12Iso, Mat12Ard, Mat32Iso, Mat32Ard, Mat52Iso, Mat52Ard, MeanZero, MeanConst, MeanLin, MeanPoly, SumMean, ProdMean, optimize!
 
 typealias MatF64 AbstractMatrix{Float64}
 typealias VecF64 AbstractVector{Float64}
