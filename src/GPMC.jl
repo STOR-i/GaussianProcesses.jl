aed22c38b4c682e714fd5a0f3c100cd6ff80b21b
nemethis <christopher.nemeth@hotmail.com>
2016-11-30 17:32:42 +0000

    fixed minor typo

--- src/GPMC.jl
+++ src/GPMC.jl
@@ -161,7 +161,7 @@ end
 #dlog p(θ,v|y) = dlog p(y|v,θ) + dlog p(v) +  dlog p(θ)
 function dlog_posterior(gp::GPMC, Kgrad::MatF64; lik::Bool=false, mean::Bool=true, kern::Bool=true)
     update_ll_and_dll!(gp::GPMC, Kgrad; lik=lik, mean=mean, kern=kern)
-    gp.dll + [-gp.v;zeros(num_params(gp.lik)+num_params(gp.m)++num_params(gp.k))]   #+ dlog_prior()
+    gp.dll + [-gp.v;zeros(num_params(gp.lik)+num_params(gp.m)+num_params(gp.k))]   #+ dlog_prior()
 end    
 
 
