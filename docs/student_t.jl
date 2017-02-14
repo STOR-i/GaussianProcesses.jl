cd1954a21f954d2509cf373ae9d7930b23413f54
nemethis <christopher.nemeth@hotmail.com>
2017-02-14 10:03:59 +0000

    updated example

--- docs/student_t.jl
+++ docs/student_t.jl
@@ -5,7 +5,7 @@ using GaussianProcesses
 
 n = 20
 X = linspace(-3,3,n)
-sigma = 2.0
+sigma = 1.0
 Y = X + sigma*rand(Distributions.TDist(3),n)
 
 #plot the data
@@ -22,20 +22,13 @@ GaussianProcesses.set_priors!(gp.lik,[Distributions.Normal(-2.0,4.0)])
 GaussianProcesses.set_priors!(gp.k,[Distributions.Normal(-2.0,4.0),Distributions.Normal(-2.0,4.0)])
 
 optimize!(gp)
-xtest = collect(linspace(-4.0,4.0,20));
-fmean, fvar = predict(gp,xtest);
-
-
-plot(layer(x=xtest,y=fmean,ymin=exp(fmean-1.96sqrt(fvar)),ymax=exp(fmean+1.96sqrt(fvar)),Geom.line,Geom.ribbon),layer(x=X,y=Y,Geom.point))
 
 #MCMC
 samples = mcmc(gp;mcrange=Klara.BasicMCRange(nsteps=50000, thinning=10, burnin=10000))
 
 plot(y=samples[end,:],Geom.line) #check MCMC mixing
 
-
-########
-
+########################################################
 #Plot posterior samples
 xtest = linspace(minimum(gp.X),maximum(gp.X),50);
 ymean = [];
@@ -47,31 +40,21 @@ for i in 1:size(samples,2)
     push!(fsamples,rand(gp,xtest))
 end
 
-
-layers = []
-for f in fsamples
-    push!(layers, layer(x=xtest,y=f,Geom.line))
-end
-
-plot(layers...,Guide.xlabel("X"),Guide.ylabel("f"))
-
 quant = Array(Float64,50,2);
 for i in 1:50
-    quant[i,:] = quantile(rateSamples[i,:],[0.05,0.95])
+    quant[i,:] = quantile(fsamples[i],[0.025,0.975])
 end
 
-plot(layer(x=xtest,y=fmean,ymin=quant[:,1],ymax=quant[:,2],Geom.line,Geom.ribbon),layer(x=vec(X),y=Y,Geom.point))
+plot(layer(x=xtest,y=mean(fsamples),ymin=quant[:,1],ymax=quant[:,2],Geom.line,Geom.ribbon),layer(x=vec(X),y=Y,Geom.point))
 
 ################################
 #Predict
-
 layers = []
 for ym in ymean
     push!(layers, layer(x=xtest,y=ym,Geom.line))
 end
 
 plot(layers...,Guide.xlabel("X"),Guide.ylabel("y"))
-
 
 plot(layer(x=xtest,y=mean(ymean),Geom.line),
      layer(x=X,y=Y,Geom.point))
