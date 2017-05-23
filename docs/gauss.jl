6e6bc9ef81546e0aa95b71efe918bd38f04542c1
nemethis <christopher.nemeth@hotmail.com>
2017-05-23 22:39:02 +0100

    almost fixed likelihood parameter optimisation

--- docs/gauss.jl
+++ docs/gauss.jl
@@ -9,7 +9,7 @@ Y = sin(X) + sigma*randn(n)
 
 #build the model
 m = MeanZero()
-k = Matern(3/2,0.0,0.0)
+k = SE(0.0,0.0)
 l = GaussLik(log(sigma))
 
 gp1 = GP(X', vec(Y), m, k, log(sigma))
@@ -34,4 +34,32 @@ y2 = predict_y(gp2,xtest)[1]
 plot(layer(x=X,y=Y,Geom.point),
      layer(x=xtest,y=y1,Geom.line),
      layer(x=xtest,y=y2,Geom.line))
+
+
+#likelihood parameters for gp2 are not converging to the correct answer, but are close. Seems to be an issue with the optimizer. I think the stopping condition needs to be depend on the magnitude of the gradient.
+
+init_params = GaussianProcesses.get_params(gp1)
+
+function ff(hyp)
+    GaussianProcesses.set_params!(gp1, hyp)
+    GaussianProcesses.update_target!(gp1)
+    return gp1.target
+end
+
+GaussianProcesses.update_target_and_dtarget!(gp1)
+theor_grad = copy(gp1.dtarget)
+numer_grad = Calculus.gradient(ff, init_params)
+
+
+init_params = GaussianProcesses.get_params(gp2)
+
+function fg(hyp)
+    GaussianProcesses.set_params!(gp2, hyp)
+    GaussianProcesses.update_target!(gp2)
+    return gp2.target
+end
+
+GaussianProcesses.update_target_and_dtarget!(gp2)
+theor_grad = copy(gp2.dtarget)
+numer_grad = Calculus.gradient(fg, init_params)
 
