9f55d223ccccfcdfc1787694e149391cb28cd74e
nemethis <christopher.nemeth@hotmail.com>
2017-07-25 12:44:57 +0100

    minor typos

--- src/mcmc.jl
+++ src/mcmc.jl
@@ -2,18 +2,17 @@
 # Description
 A function for running a variety of MCMC algorithms for estimating the GP hyperparameters. This function uses the MCMC algorithms provided by the Klara package and the user is referred to this package for further details.
 
-# Arguments:
-* `gp::GP`: Predefined Gaussian process type
-* `start::Vector{Float64}`: Select a starting value, default is taken as current GP parameters
-* `sampler::Klara.MCSampler`: MCMC sampler selected from the Klara package
-* `mcrange::Klara.BasicMCRange`: Choose number of MCMC iterations and burnin length, default is nsteps=5000, burnin = 1000
-""" ->
+    # Arguments:
+    * `gp::GP`: Predefined Gaussian process type
+        * `start::Vector{Float64}`: Select a starting value, default is taken as current GP parameters
+        * `sampler::Klara.MCSampler`: MCMC sampler selected from the Klara package
+        * `mcrange::Klara.BasicMCRange`: Choose number of MCMC iterations and burnin length, default is nsteps=5000, burnin = 1000
+        """ ->
 function mcmc(gp::GPBase;
               start::Vector{Float64}=get_params(gp),
               sampler=Klara.MALA(0.1),
               mcrange=Klara.BasicMCRange(nsteps=5000, burnin=1000))
 
-    
     
     function logtarget(hyp::Vector{Float64})  #log-target
         try
@@ -51,4 +50,4 @@ function mcmc(gp::GPBase;
     set_params!(gp,start)      #reset the parameters stored in the GP to original values
     return chain.value
 end    
-    
+
