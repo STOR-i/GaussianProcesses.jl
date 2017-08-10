9c24dfe8f069914f5a4be0e25f4b34764cac7804
nemethis <christopher.nemeth@hotmail.com>
2017-08-10 15:36:11 +0100

    fixed test_gpmc.jl

--- src/mcmc.jl
+++ src/mcmc.jl
@@ -4,15 +4,10 @@ A function for running a variety of MCMC algorithms for estimating the GP hyperp
 
     # Arguments:
     * `gp::GP`: Predefined Gaussian process type
-        * `start::Vector{Float64}`: Select a starting value, default is taken as current GP parameters
         * `sampler::Klara.MCSampler`: MCMC sampler selected from the Klara package
         * `mcrange::Klara.BasicMCRange`: Choose number of MCMC iterations and burnin length, default is nsteps=5000, burnin = 1000
         """ ->
-function mcmc(gp::GPBase;
-              start::Vector{Float64}=get_params(gp),
-              sampler=Klara.MALA(0.1),
-              mcrange=Klara.BasicMCRange(nsteps=5000, burnin=1000))
-
+function mcmc(gp::GPBase)
     
     function logtarget(hyp::Vector{Float64})  #log-target
         try
@@ -35,17 +30,33 @@ function mcmc(gp::GPBase;
     end
 
     function dlogtarget(hyp::Vector{Float64}) #gradient of the log-target
-        set_params!(gp, hyp)
-        return update_target_and_dtarget!(gp)
+        try
+            set_params!(gp, hyp)
+            return update_target_and_dtarget!(gp)
+        catch err
+            if !all(isfinite.(hyp))
+                println(err)
+                return -Inf
+            elseif isa(err, ArgumentError)
+                println(err)
+                return -Inf
+            elseif isa(err, Base.LinAlg.PosDefException)
+                println(err)
+                return -Inf
+            else
+                throw(err)
+            end
+        end        
     end
     
+    start = get_params(gp)
     starting = Dict(:p=>start)
-    q = BasicContMuvParameter(:p, logtarget=logtarget, gradlogtarget=dlogtarget) 
-    model = likelihood_model(q, false)               #set-up the model
-    tune = AcceptanceRateMCTuner(0.6, verbose=true)  #set length of tuning (default to burnin length)
-    job = BasicMCJob(model, sampler, mcrange, starting)   #set-up MCMC job
+    q = Klara.BasicContMuvParameter(:p, logtarget=logtarget, gradlogtarget=dlogtarget) 
+    model = Klara.likelihood_model(q, false)               #set-up the model
+    tune = Klara.AcceptanceRateMCTuner(0.6, verbose=true)  #set length of tuning (default to burnin length)
+    job = Klara.BasicMCJob(model, Klara.MALA(0.1), Klara.BasicMCRange(nsteps=5000, burnin=1000), starting,tuner=tune)   #set-up MCMC job
     print(job)                                #display MCMC set-up for the user
-    run(job)
+    Klara.run(job)
     chain = Klara.output(job)
     set_params!(gp,start)      #reset the parameters stored in the GP to original values
     return chain.value
