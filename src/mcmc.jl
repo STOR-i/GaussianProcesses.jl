9b38cbc19e4f3685a7b6f5cf8a96da82b247dc6a
nemethis <christopher.nemeth@hotmail.com>
2017-08-16 17:43:01 +0100

    improved hmc sampler

--- src/mcmc.jl
+++ src/mcmc.jl
@@ -8,31 +8,48 @@ A function for running a variety of MCMC algorithms for estimating the GP hyperp
         * `ε::Real`: Stepsize parameter
         * `L::Int`: Number of leapfrog steps
         """ ->
-function mcmc(gp::GPBase; nIter::Int=1000, ε::Float64=0.05, L::Int=5)
+function mcmc(gp::GPBase; nIter::Int=1000, ε::Real=0.01, Lmin::Int=5, Lmax::Int=15)
 
-    θ = get_params(gp)
-    D = length(θ)
+    function calc_target(gp::GPBase,θ::Vector{Float64}) #log-target and its gradient 
+        try
+            set_params!(gp, θ)
+            return update_target_and_dtarget!(gp)
+        catch err
+            if !all(isfinite.(θ))
+                println(err)
+                return -Inf
+            elseif isa(err, ArgumentError)
+                println(err)
+                return -Inf
+            elseif isa(err, Base.LinAlg.PosDefException)
+                println(err)
+                return -Inf
+            else
+                throw(err)
+            end
+        end        
+    end
+
+
+    θ_cur = get_params(gp)
+    D = length(θ_cur)
     post = Array{Float64}(nIter,D)     #posterior samples
-    post[1,:] = θ
+    post[1,:] = θ_cur
     
     update_target_and_dtarget!(gp)
     target_cur, grad_cur = -gp.target, -gp.dtarget
-    grad = grad_cur
-    target = target_cur
     
     for t in 1:nIter
+        θ, target, grad = θ_cur, target_cur, grad_cur
         
-        ν_cur = randn(D)
-
-        ν = ν_cur + 0.5 * ε * grad_cur
+        ν_cur = randn(D)        
+        ν = ν_cur + 0.5 * ε * grad
         reject = false
-        for l in 1:L
+        for l in 1:rand(Lmin:Lmax)
             θ += ε * ν
-            set_params!(gp,θ)
-            try
-                update_target_and_dtarget!(gp)
-            catch
-                reject =true
+            calc_target(gp,θ)
+            if any(isnan.(gp.dtarget))
+                reject=true
                 break
             end
             target, grad = -gp.target, -gp.dtarget
@@ -42,19 +59,19 @@ function mcmc(gp::GPBase; nIter::Int=1000, ε::Float64=0.05, L::Int=5)
 
         if reject
             post[t,:] = θ_cur
-            θ = θ_cur
-        end
-        
-        α = target - 0.5 * ν'ν - target_cur + 0.5 * ν_cur'ν_cur
-        u = log(rand())
+        else        
+            α = -target - 0.5 * ν'ν + target_cur + 0.5 * ν_cur'ν_cur
+            u = log(rand())
 
-        if u < α 
-            θ_cur = θ
-            target_cur = target
-            grad_cur = grad
+            if u < α 
+                θ_cur = θ
+                target_cur = target
+                grad_cur = grad
+            end
+            post[t,:] = θ_cur
         end
-        post[t,:] = θ
     end
     return post
 end    
+
 
