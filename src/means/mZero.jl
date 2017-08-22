d59d847f767735908dec9c500a0e1dac51d457e8
Jamie Fairbrother <jfairbrother87@hotmail.com>
2017-08-22 17:29:09 +0100

    Fixed prior related bugs in MeanZero

--- src/means/mZero.jl
+++ src/means/mZero.jl
@@ -1,17 +1,15 @@
-#Zero mean function
+# Zero mean function
 
 @doc """
 # Description
 Constructor for the zero mean function
 
-m(x) = 0
+m() = 0
 """ ->
 type MeanZero <: Mean
-    β::Float64
-    MeanZero() = new(0.0)
 end
 
-mean(mZero::MeanZero,x::MatF64) =  fill(0.0, size(x,2))
-
-get_params(mZero::MeanZero) = []
 num_params(mZero::MeanZero) = 0
+mean(mZero::MeanZero, x::MatF64) =  fill(0.0, size(x,2))
+get_params(mZero::MeanZero) = Float64[]
+get_priors(mZero::MeanZero) = []
