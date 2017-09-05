ffb2a40a914088af218a760f2ef9fb296d5beb5b
Jamie Fairbrother <jfairbrother87@hotmail.com>
2017-09-05 16:53:52 +0100

    Modified optimize! to allow user to specify which hyperparameters are optimized

--- src/optimize.jl
+++ src/optimize.jl
@@ -1,3 +1,6 @@
+get_params_kwargs(::Type{GPE}; kwargs...) = delete!(Dict(kwargs), :lik)
+get_params_kwargs{T<:Real}(::Type{GPMC{T}}; kwargs...) = delete!(Dict(kwargs), :noise)
+
 @doc """
     # Description
     A function for optimising the GP hyperparameters based on type II maximum likelihood estimation. This function performs gradient based optimisation using the Optim pacakge to which the user is referred to for further details.
@@ -6,25 +9,29 @@
     * `gp::GPBase`: Predefined Gaussian process type
     * `mean::Bool`: Mean function hyperparameters should be optmized
     * `kern::Bool`: Kernel function hyperparameters should be optmized
+    * `noise::Bool`: Observation noise hyperparameter should be optimized (GPE only)
+    * `lik::Bool`: Likelihood hyperparameters should be optimized (GPMC only)
     * `kwargs`: Keyword arguments for the optimize function from the Optim package
 
     # Return:
     * `::Optim.MultivariateOptimizationResults{Float64,1}`: optimization results object
     """ 
-function optimize!(gp::GPBase; method=LBFGS(), kwargs...)
-    func = get_optim_target(gp)
-    init = get_params(gp)  # Initial hyperparameter values
-    results = optimize(func,init; method=method, kwargs...)      # Run optimizer
-    set_params!(gp, Optim.minimizer(results))
+function optimize!(gp::GPBase; method=LBFGS(), mean::Bool=true, kern::Bool=true, noise::Bool=true, lik::Bool=true, kwargs...)
+    params_kwargs = get_params_kwargs(typeof(gp); mean=mean, kern=kern, noise=noise, lik=lik)
+    println(params_kwargs)
+    func = get_optim_target(gp; params_kwargs...)
+    init = get_params(gp; params_kwargs...)  # Initial hyperparameter values
+    results = optimize(func, init; method=method, kwargs...)     # Run optimizer
+    set_params!(gp, Optim.minimizer(results); params_kwargs...)
     update_target!(gp)
     return results
 end
-
-function get_optim_target(gp::GPBase)
+    
+function get_optim_target(gp::GPBase; params_kwargs...)
     
     function ltarget(hyp::Vector{Float64})
         try
-            set_params!(gp, hyp)
+            set_params!(gp, hyp; params_kwargs...)
             update_target!(gp)
             return -gp.target
         catch err
@@ -45,8 +52,8 @@ function get_optim_target(gp::GPBase)
 
     function ltarget_and_dltarget!(hyp::Vector{Float64}, grad::Vector{Float64})
         try
-            set_params!(gp, hyp)
-            update_target_and_dtarget!(gp)
+            set_params!(gp, hyp; params_kwargs...)
+            update_target_and_dtarget!(gp; params_kwargs...)
             grad[:] = -gp.dtarget
             return -gp.target
         catch err
@@ -62,7 +69,7 @@ function get_optim_target(gp::GPBase)
             else
                 throw(err)
             end
-        end 
+        end
     end
     
     function dltarget!(hyp::Vector{Float64}, grad::Vector{Float64})
@@ -72,3 +79,4 @@ function get_optim_target(gp::GPBase)
     func = OnceDifferentiable(ltarget, dltarget!, ltarget_and_dltarget!)
     return func
 end
+
