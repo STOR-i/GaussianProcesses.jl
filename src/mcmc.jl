bc606b473a0ef7cfff5772f5d995d9c46da74f09
maximerischard <rischardmaxime@gmail.com>
2018-03-01 15:43:20 +0000

    fix bug in HMC; make it possible in MCMC to reuse covariance matrix and Cholesky

--- src/mcmc.jl
+++ src/mcmc.jl
@@ -8,12 +8,18 @@ A function for running a variety of MCMC algorithms for estimating the GP hyperp
         * `ε::Real`: Stepsize parameter
         * `L::Int`: Number of leapfrog steps
         """ ->
-function mcmc(gp::GPBase; nIter::Int=1000, ε::Real=0.01, Lmin::Int=5, Lmax::Int=15)
+function mcmc(gp::GPBase; nIter::Int=1000, ε::Float64=0.01, Lmin::Int=5, Lmax::Int=15,
+        lik::Bool=true,
+        domean::Bool=true,
+        kern::Bool=true)
 
-    function calc_target(gp::GPBase,θ::Vector{Float64}) #log-target and its gradient 
+    count = 0
+    Kgrad = Array{Float64}( gp.nobsv, gp.nobsv)
+    function calc_target(gp::GPBase, θ::Vector{Float64}) #log-target and its gradient 
+        count += 1
         try
-            set_params!(gp, θ)
-            update_target_and_dtarget!(gp)
+            set_params!(gp, θ; lik=lik, domean=domean, kern=kern)
+            update_target_and_dtarget!(gp, Kgrad; lik=lik, domean=domean, kern=kern)
             return true
         catch err
             if !all(isfinite.(θ))
@@ -29,14 +35,15 @@ function mcmc(gp::GPBase; nIter::Int=1000, ε::Real=0.01, Lmin::Int=5, Lmax::Int
     end
 
 
-    θ_cur = get_params(gp)
+    θ_cur = get_params(gp; lik=lik, domean=domean, kern=kern)
     D = length(θ_cur)
     post = Array{Float64}(nIter,D)     #posterior samples
     post[1,:] = θ_cur
     
-    update_target_and_dtarget!(gp)
-    target_cur, grad_cur = -gp.target, -gp.dtarget
+    @assert calc_target(gp, θ_cur)
+    target_cur, grad_cur = gp.target, gp.dtarget
     
+    num_acceptances = 0
     for t in 1:nIter
         θ, target, grad = θ_cur, target_cur, grad_cur
         
@@ -50,7 +57,7 @@ function mcmc(gp::GPBase; nIter::Int=1000, ε::Real=0.01, Lmin::Int=5, Lmax::Int
                 reject=true
                 break
             end
-            target, grad = -gp.target, -gp.dtarget
+            target, grad = gp.target, gp.dtarget
             ν += ε * grad
         end
         ν -= 0.5*ε * grad
@@ -58,10 +65,11 @@ function mcmc(gp::GPBase; nIter::Int=1000, ε::Real=0.01, Lmin::Int=5, Lmax::Int
         if reject
             post[t,:] = θ_cur
         else        
-            α = -target - 0.5 * ν'ν + target_cur + 0.5 * ν_cur'ν_cur
+            α = target - 0.5 * ν'ν - target_cur + 0.5 * ν_cur'ν_cur
             u = log(rand())
 
             if u < α 
+                num_acceptances += 1
                 θ_cur = θ
                 target_cur = target
                 grad_cur = grad
@@ -69,6 +77,9 @@ function mcmc(gp::GPBase; nIter::Int=1000, ε::Real=0.01, Lmin::Int=5, Lmax::Int
             post[t,:] = θ_cur
         end
     end
+    set_params!(gp, θ_cur; lik=lik, domean=domean, kern=kern)
+    println("number of function calls: ", count)
+    @printf("number of acceptances: %d / %d \n", num_acceptances, nIter)
     return post'
 end    
 
