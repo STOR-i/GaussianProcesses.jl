1f37d16b3a8f60df2358d920a7fcbe462c93d37d
David Widmann <dev+github@devmotion.de>
2018-08-31 04:14:47 +0200

    Update periodic kernel

--- src/kernels/periodic.jl
+++ src/kernels/periodic.jl
@@ -29,21 +29,24 @@ mutable struct Periodic <: Isotropic{Euclidean}
         new(exp(2 * ll), exp(2 * lσ), exp(lp), [])
 end
 
-get_params(pe::Periodic) = Float64[log(pe.ℓ2)/2.0, log(pe.σ2)/2.0, log(pe.p)]
+get_params(pe::Periodic) = Float64[log(pe.ℓ2) / 2, log(pe.σ2) / 2, log(pe.p)]
 get_param_names(pe::Periodic) = [:ll, :lσ, :lp]
 num_params(pe::Periodic) = 3
 
 function set_params!(pe::Periodic, hyp::VecF64)
     length(hyp) == 3 || throw(ArgumentError("Periodic function has only three parameters"))
-    pe.ℓ2, pe.σ2 = exp.(2.0*hyp[1:2])
+    pe.ℓ2, pe.σ2 = exp(2 * hyp[1]), exp(2 * hyp[2])
     pe.p = exp(hyp[3])
 end
 
-Statistics.cov(pe::Periodic, r::Float64) = pe.σ2*exp(-2.0/pe.ℓ2*sin(π*r/pe.p)^2)
+Statistics.cov(pe::Periodic, r::Float64) = pe.σ2 * exp(-2 / pe.ℓ2 * sin(π * r / pe.p)^2)
 
 
-@inline dk_dll(pe::Periodic, r::Float64) = 4.0*pe.σ2*(sin(π*r/pe.p)^2/pe.ℓ2)*exp(-2.0/pe.ℓ2*sin(π*r/pe.p)^2)  # dK_dlogℓ
-@inline dk_dlp(pe::Periodic, r::Float64) = 4.0/pe.ℓ2*pe.σ2*(π*r/pe.p)*sin(π*r/pe.p)*cos(π*r/pe.p)*exp(-2/pe.ℓ2*sin(π*r/pe.p)^2)    # dK_dlogp
+@inline dk_dll(pe::Periodic, r::Float64) =
+    (s = 2 * sin(π * r / pe.p)^2 / pe.ℓ2; 2 * pe.σ2 * s * exp(-s)) # dK_dlogℓ
+@inline dk_dlp(pe::Periodic, r::Float64) =
+    (s = π * r / pe.p; t = 2 / pe.ℓ2; pe.σ2 * s * t * sin(2 * s) * exp(-t * sin(s)^2))  # dK_dlogp
+
 @inline function dk_dθp(pe::Periodic, r::Float64, p::Int)
     if p==1
         dk_dll(pe, r)
