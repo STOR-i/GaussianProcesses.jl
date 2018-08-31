6320f4a2f39500f0847fb89d90b196e94472321e
David Widmann <dev+github@devmotion.de>
2018-08-31 04:12:47 +0200

    Update Noise

--- src/kernels/noise.jl
+++ src/kernels/noise.jl
@@ -23,24 +23,21 @@ mutable struct Noise <: Kernel
     Noise(lσ::Float64) = new(exp(2 * lσ), [])
 end
 
-Statistics.cov(noise::Noise, sameloc::Bool) = noise.σ2*sameloc
-function Statistics.cov(noise::Noise, x::VecF64, y::VecF64)
-    return cov(noise, (norm(x-y)<eps()))
-end
+Statistics.cov(noise::Noise, sameloc::Bool) = sameloc ? noise.σ2 : 0.0
+Statistics.cov(noise::Noise, x::VecF64, y::VecF64) = cov(noise, euclidean(x, y) < eps())
 
-get_params(noise::Noise) = Float64[log(noise.σ2)/2.0]
+get_params(noise::Noise) = Float64[log(noise.σ2) / 2]
 get_param_names(noise::Noise) = [:lσ]
 num_params(noise::Noise) = 1
 
 function set_params!(noise::Noise, hyp::VecF64)
     length(hyp) == 1 || throw(ArgumentError("Noise kernel only has one parameter"))
-    noise.σ2 = exp(2.0*hyp[1])
+    noise.σ2 = exp(2 * hyp[1])
 end
 
-@inline dk_dlσ(noise::Noise, sameloc=Bool) = 2.0*cov(noise,sameloc)
-@inline function dKij_dθp(noise::Noise, X::MatF64, i::Int, j::Int, p::Int, dim::Int)
-    return dk_dlσ(noise, norm(X[:,i]-X[:,j])<eps())
-end
+dKij_dθp(noise::Noise, X::MatF64, i::Int, j::Int, p::Int, dim::Int) =
+    2 * cov(noise, view(X, :, i), view(X, :, j))
+
 @inline function dKij_dθp(noise::Noise, X::MatF64, data::EmptyData, i::Int, j::Int, p::Int, dim::Int)
     return dKij_dθp(noise, X, i, j, p, dim)
 end
