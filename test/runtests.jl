fa0f925d3c4ed9627c0ad875c16d3ef7a2cf1a4d
maximerischard <rischardmaxime@gmail.com>
2018-11-02 12:49:42 +0000

    clean up order of GPE constructors

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
 module TestGaussianProcesses
 @time include("utils.jl")
+@time include("gp.jl")
 @time include("means.jl")
 @time include("kernels.jl")
-@time include("gp.jl")
 @time include("optim.jl")
 @time include("mcmc.jl")
 @time include("gpmc.jl")
