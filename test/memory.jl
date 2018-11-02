10070fb3c895a12a43123d2a4ffd9f7e1beb8d23
maximerischard <rischardmaxime@gmail.com>
2018-11-02 11:47:28 +0000

    fix bit/byte mistake in memory allocation test

--- test/memory.jl
+++ test/memory.jl
@@ -13,10 +13,10 @@ using GaussianProcesses: EmptyData
     mem = @allocated GP(X, y, m, k, logNoise)
     # there should be 1 allocation of an n×x matrix for the covariance
     # one for its Cholesky decomposition, and one for KernelData
-    @test mem < 3.5*(nobs^2)*64
+    @test mem < 3.5*(nobs^2)*64/8
     GaussianProcesses.update_target_and_dtarget!(gp)
     mem = @allocated GaussianProcesses.update_target_and_dtarget!(gp)
-    @test mem < 0.5*nobs^2*64
+    @test mem < 0.5*nobs^2*64/8
 end
 
 @testset "sum kernel memory allocation" begin
@@ -29,9 +29,9 @@ end
     m = MeanZero()
     gp = GP(X, y, m, k, logNoise)
     mem = @allocated GP(X, y, m, k, logNoise)
-    @test mem < 3.5*nobs^2*64
+    @test mem < 3.5*nobs^2*64/8
     mem = @allocated GaussianProcesses.update_target_and_dtarget!(gp)
-    @test mem < 0.5*nobs^2*64
+    @test mem < 0.5*nobs^2*64/8
 end
 
 @testset "memory alloc with EmptyData" begin
@@ -44,7 +44,7 @@ end
     m = MeanZero()
     gp = GPE(X, y, m, k, logNoise, EmptyData())
     mem = @allocated GPE(X, y, m, k, logNoise)
-    @test mem < 2.5*nobs^2*64
+    @test mem < 2.5*nobs^2*64/8
     mem = @allocated GaussianProcesses.update_target_and_dtarget!(gp)
-    @test mem < 0.5*nobs^2*64
+    @test mem < 0.5*nobs^2*64/8
 end
