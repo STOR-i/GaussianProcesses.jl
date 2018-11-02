17f139bdd97e081c42c7a1235a46743cff03e992
maximerischard <rischardmaxime@gmail.com>
2018-11-02 11:52:13 +0000

    remove extraneous covariance computation in GPE constructor

--- test/memory.jl
+++ test/memory.jl
@@ -13,10 +13,11 @@ using GaussianProcesses: EmptyData
     mem = @allocated GP(X, y, m, k, logNoise)
     # there should be 1 allocation of an n×x matrix for the covariance
     # one for its Cholesky decomposition, and one for KernelData
-    @test mem < 3.5*(nobs^2)*64/8
+    matrix_bytes = (nobs^2)*64/8
+    @test mem/matrix_bytes < 3.5
     GaussianProcesses.update_target_and_dtarget!(gp)
     mem = @allocated GaussianProcesses.update_target_and_dtarget!(gp)
-    @test mem < 0.5*nobs^2*64/8
+    @test mem/matrix_bytes < 0.5
 end
 
 @testset "sum kernel memory allocation" begin
@@ -29,9 +30,10 @@ end
     m = MeanZero()
     gp = GP(X, y, m, k, logNoise)
     mem = @allocated GP(X, y, m, k, logNoise)
-    @test mem < 3.5*nobs^2*64/8
+    matrix_bytes = (nobs^2)*64/8
+    @test mem/matrix_bytes < 3.5
     mem = @allocated GaussianProcesses.update_target_and_dtarget!(gp)
-    @test mem < 0.5*nobs^2*64/8
+    @test mem/matrix_bytes < 0.5
 end
 
 @testset "memory alloc with EmptyData" begin
@@ -44,7 +46,8 @@ end
     m = MeanZero()
     gp = GPE(X, y, m, k, logNoise, EmptyData())
     mem = @allocated GPE(X, y, m, k, logNoise)
-    @test mem < 2.5*nobs^2*64/8
+    matrix_bytes = (nobs^2)*64/8
+    @test mem/matrix_bytes < 2.5
     mem = @allocated GaussianProcesses.update_target_and_dtarget!(gp)
-    @test mem < 0.5*nobs^2*64/8
+    @test mem/matrix_bytes < 0.5
 end
