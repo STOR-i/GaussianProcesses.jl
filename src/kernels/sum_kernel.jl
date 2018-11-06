b72322d48234700260addb7d47d763dfeeefde4f
maximerischard <rischardmaxime@gmail.com>
2018-11-06 09:39:50 +0000

    faster sum and prod kernels without kerneldata

--- src/kernels/sum_kernel.jl
+++ src/kernels/sum_kernel.jl
@@ -14,7 +14,7 @@ end
 
 @inline cov_ij(k::SumKernel, X1::AbstractMatrix, X2::AbstractMatrix, i::Int, j::Int, dim::Int) = cov_ij(k.kleft, X1, X2, i, j, dim) + cov_ij(k.kright, X1, X2, i, j, dim)
 @inline cov_ij(k::SumKernel, X1::AbstractMatrix, X2::AbstractMatrix, data::PairData, i::Int, j::Int, dim::Int) = cov_ij(k.kleft, X1, X2, data.data1, i, j, dim) + cov_ij(k.kright, X1, X2, data.data2, i, j, dim)
-    
+
 @inline function dKij_dθp(sumkern::SumKernel, X::AbstractMatrix, i::Int, j::Int, p::Int, dim::Int)
     np = num_params(sumkern.kleft)
     if p<=np
@@ -31,8 +31,17 @@ end
         return dKij_dθp(sumkern.kright, X, data.data2, i, j, p-np, dim)
     end
 end
+@inline @inbounds function dKij_dθ!(dK::AbstractVector, sumkern::SumKernel, X::AbstractMatrix, i::Int, j::Int, dim::Int, npars::Int)
+    npright = num_params(sumkern.kright)
+    npleft = num_params(sumkern.kleft)
+    dKij_dθ!(dK, sumkern.kright, X, i, j, dim, npars-npleft)
+    for ipar in npright:-1:1
+        dK[npleft+ipar] = dK[ipar]
+    end
+    dKij_dθ!(dK,  sumkern.kleft,  X, i, j, dim, npleft)
+end
 @inline @inbounds function dKij_dθ!(dK::AbstractVector, sumkern::SumKernel, X::AbstractMatrix, data::PairData, i::Int, j::Int, dim::Int, npars::Int)
-    npright = num_params(sumkern.kright) 
+    npright = num_params(sumkern.kright)
     npleft = num_params(sumkern.kleft)
     dKij_dθ!(dK, sumkern.kright, X, data.data2, i, j, dim, npars-npleft)
     for ipar in npright:-1:1
