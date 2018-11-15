9f7eae0194cbd5f4d2d4d03353df8b0ddaca9147
Johanni Brea <jbrea@users.noreply.github.com>
2018-11-15 21:14:56 +0100

    new docs

---
+++ docs/src/gp.md
@@ -0,0 +1,6 @@
+# Gaussian Processes 
+
+```@autodocs
+Modules = [GaussianProcesses]
+Pages = ["GP.jl", "GPE.jl", "GPMC.jl", "GPEelastic.jl", "common.jl", "mcmc.jl", "utils.jl"]
+```
--- docs/ybuild/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index","intro"],envversion:50,filenames:["index.rst","intro.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to GaussianProcesses.jl&#8217;s documentation!","&lt;no title&gt;"],titleterms:{document:0,gaussianprocess:0,indic:0,tabl:0,welcom:0}})
