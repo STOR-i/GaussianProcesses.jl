27fe0ab34352b4f285378039431159b71d6dbea4
maximerischard <rischardmaxime@gmail.com>
2019-03-04 17:46:32 +0000

    add periodic mean function and fix a few bugs along the way

--- src/GaussianProcesses.jl
+++ src/GaussianProcesses.jl
@@ -10,7 +10,7 @@ import Statistics: mean, cov
 # Functions that should be available to package
 # users should be explicitly exported here
 export GPBase, GP, GPE, GPMC, ElasticGPE, predict_f, predict_y, Kernel, Likelihood, CompositeKernel, SumKernel, ProdKernel, Masked, FixedKernel, fix, Noise, Const, SE, SEIso, SEArd, Periodic, Poly, RQ, RQIso, RQArd, Lin, LinIso, LinArd, Matern, Mat12Iso, Mat12Ard, Mat32Iso, Mat32Ard, Mat52Iso, Mat52Ard, #kernel functions
-    MeanZero, MeanConst, MeanLin, MeanPoly, SumMean, ProdMean, #mean functions
+    MeanZero, MeanConst, MeanLin, MeanPoly, SumMean, ProdMean, MeanPeriodic, #mean functions
     GaussLik, BernLik, ExpLik, StuTLik, PoisLik, BinLik,       #likelihood functions
     mcmc, optimize!,                                           #inference functions
     set_priors!,set_params!, update_target!, autodiff
