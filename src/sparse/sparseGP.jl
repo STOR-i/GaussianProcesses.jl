b9017ccf81ab1c7b2e91774279e86b92e0787600
maximerischard <rischardmaxime@gmail.com>
2019-03-24 18:12:17 +0000

    steps towards ll gradient for SoR

--- src/sparse/sparseGP.jl
+++ src/sparse/sparseGP.jl
@@ -1,5 +1,19 @@
 abstract type SparsePDMat{T} <: AbstractPDMat{T} end
 abstract type SparseStrategy <: CovarianceStrategy end
 
+function get_ααinvcKI!(ααinvcKI::AbstractMatrix, cK::SparsePDMat, α::Vector)
+    nobs = length(α)
+    size(ααinvcKI) == (nobs, nobs) || throw(ArgumentError(
+                @sprintf("Buffer for ααinvcKI should be a %dx%d matrix, not %dx%d",
+                         nobs, nobs,
+                         size(ααinvcKI,1), size(ααinvcKI,2))))
+    # fill!(ααinvcKI, 0)
+    # @inbounds for i in 1:nobs
+        # ααinvcKI[i,i] = -1.0
+    # end
+    ααinvcKI = cK \ (-I)
+    BLAS.ger!(1.0, α, α, ααinvcKI)
+end
+
 include("subsetofregressors.jl")
 
--- src/sparse/subsetofregressors.jl
+++ src/sparse/subsetofregressors.jl
@@ -7,7 +7,7 @@
 mutable struct SubsetOfRegsPDMat{T,M<:AbstractMatrix,PD<:AbstractPDMat{T},M2<:AbstractMatrix{T}} <: SparsePDMat{T}
     inducing::M
     ΣQR_PD::PD
-    Kuu::M2
+    Kuu::PD
     Kuf::M2
     logNoise::Float64
 end
@@ -20,7 +20,7 @@ size(a::SubsetOfRegsPDMat, d::Int) = size(a.Kuf,2)
         Σ⁻¹ = σ⁻²I - σ⁻⁴ Kuf'(Kuu + σ⁻² Kuf Kuf')⁻¹ Kuf
             = σ⁻²I - σ⁻⁴ Kuf'(       ΣQR        )⁻¹ Kuf
 """
-function \(a::SubsetOfRegsPDMat, x::AbstractArray)
+function \(a::SubsetOfRegsPDMat, x)
     return exp(-2*a.logNoise)*x - exp(-4*a.logNoise)*a.Kuf'*(a.ΣQR_PD \ (a.Kuf * x))
 end
 logdet(a::SubsetOfRegsPDMat) = logdet(a.ΣQR_PD) - logdet(a.Kuu) + 2*a.logNoise*size(a,1)
@@ -41,17 +41,24 @@ function alloc_cK(covstrat::SubsetOfRegsStrategy, nobs)
     inducing = covstrat.inducing
     ninducing = size(inducing, 2)
     Kuu  = Matrix{Float64}(undef, ninducing, ninducing)
+    chol_uu = Matrix{Float64}(undef, ninducing, ninducing)
+    Kuu_PD = PDMats.PDMat(Kuu, Cholesky(chol_uu, 'U', 0))
     Kuf  = Matrix{Float64}(undef, ninducing, nobs)
     ΣQR  = Matrix{Float64}(undef, ninducing, ninducing)
     chol = Matrix{Float64}(undef, ninducing, ninducing)
     cK = SubsetOfRegsPDMat(inducing, 
                             PDMats.PDMat(ΣQR, Cholesky(chol, 'U', 0)), # ΣQR_PD
-                            Kuu, Kuf, 42.0)
+                            Kuu_PD, Kuf, 42.0)
     return cK
 end
-function update_cK!(covstrat::SubsetOfRegsStrategy, cK::SubsetOfRegsPDMat, x::AbstractMatrix, kernel::Kernel, logNoise::Real, data::KernelData)
+function update_cK!(cK::SubsetOfRegsPDMat, x::AbstractMatrix, kernel::Kernel, 
+                    logNoise::Real, data::KernelData, covstrat::SubsetOfRegsStrategy)
     inducing = covstrat.inducing
-    Kuu = cov!(cK.Kuu, kernel, inducing)
+    Kuu = cK.Kuu
+    Kuubuffer = mat(Kuu)
+    cov!(Kuubuffer, kernel, inducing)
+    Kuubuffer, chol = make_posdef!(Kuubuffer, cholfactors(cK.Kuu))
+    Kuu_PD = wrap_cK(cK.Kuu, Kuubuffer, chol)
     Kuf = cov!(cK.Kuf, kernel, inducing, x)
     Kfu = Kuf'
     
@@ -60,7 +67,27 @@ function update_cK!(covstrat::SubsetOfRegsStrategy, cK::SubsetOfRegsPDMat, x::Ab
     
     ΣQR, chol = make_posdef!(ΣQR, cholfactors(cK.ΣQR_PD))
     ΣQR_PD = wrap_cK(cK.ΣQR_PD, ΣQR, chol)
-    return wrap_cK(cK, inducing, ΣQR_PD, Kuu, Kuf, logNoise)
+    return wrap_cK(cK, inducing, ΣQR_PD, Kuu_PD, Kuf, logNoise)
+end
+
+
+function dmll_kern!(dmll::AbstractVector, k::Kernel, X::AbstractMatrix, cK::SubsetOfRegsPDMat, data::KernelData, ααinvcKI::AbstractMatrix, covstrat::SubsetOfRegsStrategy)
+    dim, nobs = size(X)
+    inducing = covstrat.inducing
+    ninducing = size(inducing, 2)
+    nparams = num_params(k)
+    @assert nparams == length(dmll)
+    dK_buffer = Vector{Float64}(undef, nparams)
+    dmll[:] .= 0.0
+    Kuf = cK.Kuf
+    Kuu = cK.Kuu
+    ∂Kuu = similar(mat(Kuu))
+    ∂Kfu = Matrix{Float64}(undef, nobs, ninducing)
+    for iparam in 1:nparams
+        grad_slice!(∂Kuu, k, inducing, inducing, EmptyData(), iparam)
+        grad_slice!(∂Kfu, k, X, inducing,        EmptyData(), iparam)
+    end
+    return dmll
 end
 
 """
@@ -73,6 +100,12 @@ end
               x: prediction (test) locations
               f: training (observed) locations
               u: inducing point locations
+
+    We have
+        Σ ≈ Kuf' Kuu⁻¹ Kuf + σ²I
+    By Woodbury
+        Σ⁻¹ = σ⁻²I - σ⁻⁴ Kuf'(Kuu + σ⁻² Kuf Kuf')⁻¹ Kuf
+            = σ⁻²I - σ⁻⁴ Kuf'(       ΣQR        )⁻¹ Kuf
 
     The predictive mean can be derived (assuming zero mean function for simplicity)
     μ = Qxf (Qff + σ²I)⁻¹ y
