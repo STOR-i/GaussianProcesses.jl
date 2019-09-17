eee4270733270b5bbd677f85dd91e6bb3b2ad664
thomaspinder <tompinder@live.co.uk>
2019-09-17 17:37:50 +0100

    Update RFF

--- test/test_rff.jl
+++ test/test_rff.jl
@@ -1,13 +1,7 @@
-using Optim, GaussianProcesses, Distributions, Random
-
+using Optim, GaussianProcesses, Distributions, Random, RDatasets
 Random.seed!(203617)
-n=10;                          #number of training points
-d = 2
-x = 2π * rand(n, 2);              #predictors
-y = sin.(x[:, 1]) + cos.(2*x[:, 2]) + 0.05*randn(n);   #regressors
-kern = SE([0.0, 0.0], 0.0) 
 
-function scale_ω(ω::AbstractMatrix, ℓ::AbstractArray) 
+function scale_ω(ω::AbstractMatrix, ℓ::AbstractArray)
     ωscaled = (1/ℓ).*ω
     return ωscaled
 end
@@ -19,35 +13,63 @@ mutable struct RFF
     M::Int64
     ℓ::AbstractArray
     σ::Float64
+    ∇mat::AbstractMatrix
+end
+
+function RFF(d::Int64, M::Int64, K::SEArd, N::Int64; amplitude::Float64=1.0)
+    ω = rand(Normal(), M, d) # TODO: Should this be M or 2*M
+    ℓ = ones(d)
+    grad_mat = Array{Float64}(undef, N, 2*M)
+    return RFF(d, ω, scale_ω(ω, ℓ), M, K.iℓ2, amplitude, grad_mat)
 end
 
 mutable struct SSGPR
     X::AbstractMatrix
-    y::AbstractArray    
+    y::AbstractArray
     fourier::RFF
+    noise::Float64
+    N::Int64
+    d::Int64
 end
 
-function RFF(d::Int64, M::Int64, K::SEArd; amplitude::Float64=1.0)
-    ω = rand(Normal(), M, d)
-    ℓ = ones(d)
-    return RFF(d, ω, scale_ω(ω, ℓ), M, K.iℓ2, amplitude)
-end
-
-function SSGPR(X::AbstractMatrix, y::AbstractArray, K::Kernel; M::Int64=100)
+function SSGPR(X::AbstractMatrix, y::AbstractArray, K::Kernel; M::Int64=20, noise::Float64=1.0)
     d = size(X, 2)
-    features = RFF(d, M, K)
-    GP = SSGPR(X, y, features)
+    N = size(X, 1)
+    features = RFF(d, M, K, N)
+    GP = SSGPR(X, y, features, noise, N, d)
 end
-
 
 # TODO: Possibly just pass a scalar value or a SSGPR struct
-function build_design_mat!(F::RFF, X::AbstractMatrix)
+function build_design_mat(F::RFF, X::AbstractMatrix)
     N = size(X, 1)
     ϕ_x = zeros(N, 2*F.M) # Need 2*M rows for Cos and sin
     ϕ_x[:, 1:F.M] = sin.(X*transpose(F.ω))
     ϕ_x[:, (F.M + 1):end] = cos.(X*transpose(F.ω))
-    return ϕ_x    
+    return ϕ_x
 end
 
-gp = SSGPR(x, y, kern)
-println(gp)
+function grad_l(X, gp::SSGPR)
+    N = size(X, 0)
+    if gp.fourier.∇mat
+
+
+# function
+
+crabs = dataset("MASS","crabs");              # load the data
+crabs = crabs[shuffle(1:size(crabs)[1]), :];  # shuffle the data
+
+train = crabs[1:div(end,2),:];
+
+X = convert(Matrix,train[:,7:end]);          # predictors
+ybool = Array{Bool}(undef,size(train)[1])*1.0       # response
+y = 1.5*cos.(ybool) + (1/3)*sin.(2*ybool) + rand(Normal(0, 0.1), length(ybool))
+
+mZero = MeanZero();                # Zero mean function
+kern = SE(zeros(2), 0.0);   # Matern 3/2 ARD kernel (note that hyperparameters are on the log scale)
+
+gp = SSGPR(X, y, kern)
+ϕ = build_design_mat(gp.fourier, X)
+
+# gpe = GP(X', y, mZero, kern)       #Fit the GP
+# optimize!(gpe; method=ConjugateGradient())   # Optimise the hyperparameters
+# contour(gpe)
