bd90b03cefe1426e12419aa431d3863807eda700
Ray Kim <msca8h@naver.com>
2019-12-21 14:53:19 +0900

    changed name of previous hmc routine, changed to column major order

--- src/mcmc.jl
+++ src/mcmc.jl
@@ -1,13 +1,12 @@
-using ProgressMeter
 
 """
-    mcmc(gp::GPBase; kwargs...)
+    hmc(gp::GPBase; kwargs...)
 
 Runs Hamiltonian Monte Carlo algorithm for estimating the hyperparameters of Gaussian process `GPE` and the latent function in the case of `GPA`.
 """
-function mcmc(gp::GPBase; nIter::Int=1000, burn::Int=1, thin::Int=1, ε::Float64=0.1,
-              Lmin::Int=5, Lmax::Int=15, lik::Bool=true, noise::Bool=true,
-              domean::Bool=true, kern::Bool=true)
+function hmc(gp::GPBase; nIter::Int=1000, burn::Int=1, thin::Int=1, ε::Float64=0.1,
+             Lmin::Int=5, Lmax::Int=15, lik::Bool=true, noise::Bool=true,
+             domean::Bool=true, kern::Bool=true)
     precomp = init_precompute(gp)
     params_kwargs = get_params_kwargs(gp; domean=domean, kern=kern, noise=noise, lik=lik)
     count = 0
@@ -33,8 +32,8 @@ function mcmc(gp::GPBase; nIter::Int=1000, burn::Int=1, thin::Int=1, ε::Float64
     θ_cur = get_params(gp; params_kwargs...)
     D = length(θ_cur)
     leapSteps = 0                   #accumulator to track number of leap-frog steps
-    post = Array{Float64}(undef, nIter, D)     #posterior samples
-    post[1,:] = θ_cur
+    post = Array{Float64}(undef, D, nIter)     #posterior samples
+    post[:,1] = θ_cur
 
     @assert calc_target!(gp, θ_cur)
     target_cur, grad_cur = gp.target, gp.dtarget
@@ -61,7 +60,7 @@ function mcmc(gp::GPBase; nIter::Int=1000, burn::Int=1, thin::Int=1, ε::Float64
         ν -= 0.5*ε * grad
 
         if reject
-            post[t,:] = θ_cur
+            post[:,t] = θ_cur
         else
             α = target - 0.5 * ν'ν - target_cur + 0.5 * ν_cur'ν_cur
             u = log(rand())
@@ -72,16 +71,16 @@ function mcmc(gp::GPBase; nIter::Int=1000, burn::Int=1, thin::Int=1, ε::Float64
                 target_cur = target
                 grad_cur = grad
             end
-            post[t,:] = θ_cur
+            post[:,t] = θ_cur
         end
     end
-    post = post[burn:thin:end,:]
+    post = post[:,burn:thin:end]
     set_params!(gp, θ_cur; params_kwargs...)
     @printf("Number of iterations = %d, Thinning = %d, Burn-in = %d \n", nIter,thin,burn)
     @printf("Step size = %f, Average number of leapfrog steps = %f \n", ε,leapSteps/nIter)
     println("Number of function calls: ", count)
     @printf("Acceptance rate: %f \n", num_acceptances/nIter)
-    return post'
+    return post
 end
 
 
@@ -141,20 +140,20 @@ function ess(gp::GPE; nIter::Int=1000, burn::Int=1, thin::Int=1, lik::Bool=true,
     total_proposals = 0
     θ_cur = get_params(gp; params_kwargs...)
     D = length(θ_cur)
-    post = Array{Float64}(undef, nIter, D)
+    post = Array{Float64}(undef, D, nIter)
 
     for i = 1:nIter
         θ_cur, num_proposals = sample!(θ_cur)
-        post[i,:] = θ_cur
+        post[:,i] = θ_cur
         total_proposals += num_proposals
     end
 
-    post = post[burn:thin:end,:]
+    post = post[:,burn:thin:end]
     set_params!(gp, θ_cur; params_kwargs...)
     @printf("Number of iterations = %d, Thinning = %d, Burn-in = %d \n", nIter,thin,burn)
     println("Number of function calls: ", count)
     @printf("Acceptance rate: %f \n", nIter / total_proposals)
-    return post'
+    return post
 end
 
 
