269cd2ef976845b7dc4bf291b410be965c25c2b6
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2020-03-03 19:42:12 +0800

    Allow positional arguments to Optim.optimize
    From [Optim docs](https://julianlsolvers.github.io/Optim.jl/stable/#user/config/)
    > We currently recommend the statically dispatched interface by using the Optim.Options constructor

--- src/optimize.jl
+++ src/optimize.jl
@@ -2,7 +2,7 @@ get_params_kwargs(::GPE; kwargs...) = delete!(Dict(kwargs), :lik)
 get_params_kwargs(::GPA; kwargs...) = delete!(Dict(kwargs), :noise)
 
 """
-    optimize!(gp::GPBase; kwargs...)
+    optimize!(gp::GPBase, args...; kwargs...)
 
 Optimise the hyperparameters of Gaussian process `gp` based on type II maximum likelihood estimation. This function performs gradient based optimisation using the Optim pacakge to which the user is referred to for further details.
 
@@ -14,22 +14,22 @@ Optimise the hyperparameters of Gaussian process `gp` based on type II maximum l
     * `meanbounds`: [lowerbounds, upperbounds] for the mean hyperparameters
     * `kernbounds`: [lowerbounds, upperbounds] for the kernel hyperparameters
     * `noisebounds`: [lowerbound, upperbound] for the noise hyperparameter
-    * `kwargs`: Keyword arguments for the optimize function from the Optim package
+    * `args/kwargs`: Arguments and keyword arguments for the optimize function from the Optim package https://julianlsolvers.github.io/Optim.jl/stable/#user/config/
 """
-function optimize!(gp::GPBase; method = LBFGS(), domean::Bool = true, kern::Bool = true,
-                   noise::Bool = true, lik::Bool = true, 
-                   meanbounds = nothing, kernbounds = nothing, 
+function optimize!(gp::GPBase, args...; method = LBFGS(), domean::Bool = true, kern::Bool = true,
+                   noise::Bool = true, lik::Bool = true,
+                   meanbounds = nothing, kernbounds = nothing,
                    noisebounds = nothing, likbounds = nothing, kwargs...)
     params_kwargs = get_params_kwargs(gp; domean=domean, kern=kern, noise=noise, lik=lik)
     # println(params_kwargs)
     func = get_optim_target(gp; params_kwargs...)
     init = get_params(gp; params_kwargs...)  # Initial hyperparameter values
-    if meanbounds == kernbounds == noisebounds == likbounds == nothing 
-        results = optimize(func, init; method=method, kwargs...)     # Run optimizer
+    if meanbounds == kernbounds == noisebounds == likbounds == nothing
+        results = optimize(func, init, args...; method=method, kwargs...)     # Run optimizer
     else
         lb, ub = bounds(gp, noisebounds, meanbounds, kernbounds, likbounds;
                         domean = domean, kern = kern, noise = noise, lik = lik)
-        results = optimize(func.f, func.df, lb, ub, init, Fminbox(method))
+        results = optimize(func.f, func.df, lb, ub, init, Fminbox(method), args...)
     end
     set_params!(gp, Optim.minimizer(results); params_kwargs...)
     update_target!(gp)
